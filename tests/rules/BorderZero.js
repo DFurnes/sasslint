6cc05a285efe84cc6604e3d01cade6df321c2cb6
David Furnes <david@dfurnes.com>
2015-08-05 17:09:07 -0400

    Extend tester with custom assertions.

--- tests/rules/BorderZero.js
+++ tests/rules/BorderZero.js
@@ -1,43 +1,38 @@
-import test from 'tape';
-import { testValid, testInvalid } from '../../src/util';
+import test from 'tape-catch';
+import '../assertions';
 
-test('BorderZero', function(t) {
-    t.plan(2);
+test('BorderZero - when a convention of 0 is preferred', function(t) {
+    t.plan(8);
 
-    t.test('when a convention of 0 is preferred', function(t) {
-        t.plan(8);
+    const expectZero = {
+        'BorderZero': {
+            enabled: true,
+            convention: '0',
+        }
+    };
 
-        const expectZero = {
-            'BorderZero': {
-                enabled: true,
-                convention: '0',
-            }
-        };
+    t.isValid(expectZero, 'p { }', 'when a rule is empty');
 
-        testValid(t, expectZero, 'p { }', 'when a rule is empty');
+    t.isValid(expectZero, 'p { border: 1px solid #000; }', 'has a normal border');
+    t.isValid(expectZero, 'p { border: 0; }', 'has a border of 0');
+    t.isInvalid(expectZero, 'p { border: none; }', 'has a border of none');
+    t.isInvalid(expectZero, 'p { border-top: none; }', 'has a border-top of none');
+    t.isInvalid(expectZero, 'p { border-right: none; }', 'has a border-right of none');
+    t.isInvalid(expectZero, 'p { border-bottom: none; }', 'has a border-bottom of none');
+    t.isInvalid(expectZero, 'p { border-left: none; }', 'has a border-left of none');
+});
 
-        testValid(t, expectZero, 'p { border: 1px solid #000; }', 'has a normal border');
-        testValid(t, expectZero, 'p { border: 0; }', 'has a border of 0');
-        testInvalid(t, expectZero, 'p { border: none; }', 'has a border of none');
-        testInvalid(t, expectZero, 'p { border-top: none; }', 'has a border-top of none');
-        testInvalid(t, expectZero, 'p { border-right: none; }', 'has a border-right of none');
-        testInvalid(t, expectZero, 'p { border-bottom: none; }', 'has a border-bottom of none');
-        testInvalid(t, expectZero, 'p { border-left: none; }', 'has a border-left of none');
+test('BorderZero - when a convention of none is preferred', function(t) {
+    t.plan(3);
 
-    });
+    const expectNone = {
+        'BorderZero': {
+            enabled: true,
+            convention: 'none',
+        }
+    };
 
-    t.test('when a convention of none is preferred', function(t) {
-        t.plan(3);
-
-        const expectNone = {
-            'BorderZero': {
-                enabled: true,
-                convention: 'none',
-            }
-        };
-
-        testValid(t, expectNone, '.test { border: none; }', 'has a border of none');
-        testInvalid(t, expectNone, '.test { border: 0; }', 'has a border of 0');
-        testValid(t, expectNone, '.test { border: 5px; }', 'has a normal border');
-    });
+    t.isValid(expectNone, '.test { border: none; }', 'has a border of none');
+    t.isInvalid(expectNone, '.test { border: 0; }', 'has a border of 0');
+    t.isValid(expectNone, '.test { border: 5px; }', 'has a normal border');
 });
--- tests/rules/SpaceAfterPropertyColon.js
+++ tests/rules/SpaceAfterPropertyColon.js
@@ -1,25 +1,24 @@
-import test from 'tape';
-import { testValid, testInvalid } from '../../src/util';
+import test from 'tape-catch';
+import '../assertions';
 
-test('SpaceAfterPropertyColon', function (t) {
-    t.plan(1);
+test('SpaceAfterPropertyColon - when one space is preferred', function (t) {
+    t.plan(7);
 
-    t.test('when one space is preferred', function(t) {
-        t.plan(5);
+    const expectOne = {
+        'SpaceAfterPropertyColon': {
+            enabled: true,
+            convention: 'one_space',
+        }
+    };
 
-        const expectOne = {
-            'SpaceAfterPropertyColon': {
-                enabled: true,
-                convention: 'one_space',
-            }
-        };
+    t.isInvalid(expectOne, 'p { margin:0; }', 'when the colon after a property is not followed by space');
+    t.isInvalid(expectOne, 'p { color:#eee }', 'when colon after property is not followed by space and the semicolon is missing');
+    t.isValid(expectOne, 'p { color: red; }', 'when the colon after a property is followed by a space');
+    t.isValid(expectOne, 'p { color : red; }', 'when the colon after a property is surrounded by spaces');
+    t.isInvalid(expectOne, 'p { color:  red; }', 'when the colon after a property is followed by multiple spaces');
 
-        testInvalid(t, expectOne, 'p { margin:0; }', 'when the colon after a property is not followed by space');
-        testInvalid(t, expectOne, 'p { color:#eee }', 'when colon after property is not followed by space and the semicolon is missing');
-        testValid(t, expectOne, 'p { color: red; }', 'when the colon after a property is followed by a space');
-        testValid(t, expectOne, 'p { color : red; }', 'when the colon after a property is surrounded by spaces')
-        testInvalid(t, expectOne, 'p { color:  red; }', 'when the colon after a property is followed by multiple spaces')
+    t.isValid(expectOne, `[class~='\#{$test}'] { width: 100%; }`, 'when interpolation within single quotes is followed by inline property and property name is followed by a space');
+    t.isInvalid(expectOne, `[class~='\#{$test}'] { width:100%; }`, 'when interpolation within single quotes is followed by inline property and property name is not followed by a space');
 
-        // @TODO Add more tests!
-    });
+    // @TODO Add more tests!
 });
